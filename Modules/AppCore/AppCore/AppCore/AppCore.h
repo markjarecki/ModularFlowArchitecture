0191bf221151b0a7d94c698ae262cbcdf3bf3d3e
Mark Jarecki <markjarecki@fluxom.com>
2019-03-29 12:38:32 +0100

    [ADD] AppCore
    [REMOVE] CommonCore, FlowCore, FeatureCore as they are now redundant

---
+++ Modules/AppCore/AppCore/AppCore/AppCore.h
@@ -0,0 +1,19 @@
+//
+//  AppCore.h
+//  AppCore
+//
+//  Created by Mark Jarecki on 29/3/19.
+//  Copyright © 2019 Mark Jarecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppCore.
+FOUNDATION_EXPORT double AppCoreVersionNumber;
+
+//! Project version string for AppCore.
+FOUNDATION_EXPORT const unsigned char AppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCore/PublicHeader.h>
+
+
---
+++ Modules/AppCore/AppCore/AppCore/AppCore.swift
@@ -0,0 +1,13 @@
+//
+//  AppCore.swift
+//  AppCore
+//
+//  Created by Mark Jarecki on 29/3/19.
+//  Copyright © 2019 Mark Jarecki. All rights reserved.
+//
+
+/// File is required to eliminate linker errors:
+///
+/// e.g. ld: warning: Could not find auto-linked library 'swiftFoundation'
+
+import Foundation
