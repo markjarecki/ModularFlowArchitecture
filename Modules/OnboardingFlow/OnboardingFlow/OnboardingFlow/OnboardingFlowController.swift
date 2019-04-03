3b07498e4b6d1dd7dc2a6a8edc8d751d9a13fcf8
Mark Jarecki <markjarecki@fluxom.com>
2019-04-03 15:21:24 +0200

    [UPDATE] OnboardingFlowController FlowActionDelegate into base class
    [REMOVE] Onboarding FlowActionDelegate

--- Modules/OnboardingFlow/OnboardingFlow/OnboardingFlow/OnboardingFlowController.swift
+++ Modules/OnboardingFlow/OnboardingFlow/OnboardingFlow/OnboardingFlowController.swift
@@ -13,7 +13,7 @@ import FLXFlow
 import OnboardingFirstFeature
 import OnboardingSecondFeature
 
-final public class OnboardingFlowController: StackedFlowController {
+final public class OnboardingFlowController: StackedFlowController, FlowActionDelegate {
     
     // MARK: - Initialisers
     
@@ -57,6 +57,64 @@ final public class OnboardingFlowController: StackedFlowController {
     required public init?(coder aDecoder: NSCoder) {
     
         fatalError("init(coder:) has not been implemented")
+        
+    }
+    
+    // MARK: - FlowActionDelegate conformance
+    
+    public func process(action: FlowAction) -> Bool {
+        
+        switch action {
+            
+        case OnboardingFlowAction.showSecond: showSecond()
+        case OnboardingFlowAction.returnToFirst(let feature): returnToFirst(from: feature)
+            
+        // Propagate action - event not handled
+        default: return true
+            
+        }
+        
+        // Stop propagation - event handled
+        return false
+        
+    }
+    
+    // MARK: - Private helper methods
+    
+    private func showSecond() {
+        
+        // Initialise with the Home Flow Controller
+        let secondFeature = OnboardingSecondViewController()
+        
+        // Assign flow interactors
+        secondFeature.tapFlowInteractor = OnboardingSecondFeatureToCompletionTapFlowInteractor(viewController: secondFeature){ tap, viewController in
+            
+            if tap.state == .ended {
+                
+                guard let viewController = viewController as? OnboardingSecondViewController else { return }
+                guard let flowController = viewController.flowController else { return }
+                
+                // Trigger a flow action on the flow controller
+                flowController.flow(action: OnboardingFlowAction.onboardingCompleted(from: viewController))
+                
+            }
+            
+        }
+        
+        secondFeature.edgeSwipeFlowInteractor = OnboardingSecondFeatureToFirstFeatureEdgeSwipeFlowInteractor(viewController: secondFeature)
+        
+        // Add it to the view controller stack - with default animation
+        stackedNavigationController?.pushViewController(secondFeature, animated: true)
+        
+    }
+    
+    private func returnToFirst(from: OnboardingSecondViewController) {
+        
+        // Assign as feature's edgeswipeFlowInteractor as the navigationController's transition delegate
+        stackedNavigationController?.delegate = from.edgeSwipeFlowInteractor as? UINavigationControllerDelegate
+        
+        // Return to the Onboarding First Feature, by popping the Second Feature from the stack
+        stackedNavigationController?.popViewController(animated: true)
         
     }
     
